4d139b8af0f356ea208a69a02dcf445e6289621e
Jian Chen <35271746+legendaryC@users.noreply.github.com>
2020-05-16 15:11:36 -0500

    Updates

---
+++ precache-manifest.61b97ba21dc77911cf81575fc142e0f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91752acaa90a8067031c05b4743f7648",
+    "url": "/blogV1.0/index.html"
+  },
+  {
+    "revision": "910cfbb13abfa8be000c",
+    "url": "/blogV1.0/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a43d82790d0b9668125a",
+    "url": "/blogV1.0/static/js/2.3b72b5a9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blogV1.0/static/js/2.3b72b5a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "910cfbb13abfa8be000c",
+    "url": "/blogV1.0/static/js/main.e9ac240c.chunk.js"
+  },
+  {
+    "revision": "bec08d93fa706fd0bddb",
+    "url": "/blogV1.0/static/js/runtime-main.9715726a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blogV1.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
